45cd712d0957e5c3b80e848c20330162b7070718
Nico CommandersAct <nicocommandersact@air-de-nico.home>
2020-11-04 22:51:55 +0100

    index linked

--- main.js
+++ main.js
@@ -4,3 +4,13 @@
   // do someting
 
 })();
+
+
+// (function(){
+//   let script = document.createElement('script');
+//   script.src = 'https://nicocouz.github.io/Bookmarklet/main.js';
+//   document.body.appendChild(script);
+//   let index = document.createElement('script');
+//   index.src = 'https://nicocouz.github.io/Bookmarklet/index.html';
+//   document.body.appendChild(index);
+// })();
