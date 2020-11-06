ae16509eb02d226043e72b4eda957d8310eee91d
Nico CommandersAct <nicocommandersact@air-de-nico.home>
2020-11-06 15:53:10 +0100

    remove Modal click outside

--- main.js
+++ main.js
@@ -88,18 +88,19 @@
 
     var modal = document.getElementById("myModal");
     // Get the <span> element that closes the modal
-    // var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close")[0];
     // When the user clicks the button, open the modal
     function getModal() {
       modal.style.display = "block";
     }
     // When the user clicks on <span> (x), close the modal
-    // span.onclick = function() {
-    //   modal.style.display = "none";
-    // }
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-      if (event.toElement.offsetParent.id != "myModal") {
+      if (event.target == modal) {
+        modal.style.display = "none";
         document.getElementById("myModal").remove();
         document.getElementById("Bookmarklet_main").remove();
         document.getElementById("Bookmarklet_style").remove();
