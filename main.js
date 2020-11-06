53ecfabb406006268a8051c3feb06e41ce5f1a9d
Nico CommandersAct <nicocommandersact@air-de-nico.home>
2020-11-06 15:44:51 +0100

    remove Modal click outside

--- main.js
+++ main.js
@@ -88,19 +88,18 @@
 
     var modal = document.getElementById("myModal");
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    // var span = document.getElementsByClassName("close")[0];
     // When the user clicks the button, open the modal
     function getModal() {
       modal.style.display = "block";
     }
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
+      if (event.toElement.offsetParent.id != "myModal") {
         document.getElementById("myModal").remove();
         document.getElementById("Bookmarklet_main").remove();
         document.getElementById("Bookmarklet_style").remove();
@@ -114,7 +113,7 @@
 
 // THIS IS TO ADD WITHIN THE BOOKMARKLET URL
 
-// 
+//
 // (function() {
 //   let script = document.createElement('script');
 //   script.src = 'https://nicocouz.github.io/Bookmarklet/main.js';
